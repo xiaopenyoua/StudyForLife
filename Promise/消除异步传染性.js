be1e8dc8db54459b48c3e372944a88658c31fd80
wangq <89414266@qq.com>
2023-09-05 18:03:37 +0800

    fix：消除异步传染性

--- Promise/消除异步传染性.js
+++ Promise/消除异步传染性.js
@@ -1,5 +1,5 @@
 function getdata() {
-  return fetch('./data.js')
+  return fetch('https://tenapi.cn/v2/api')
 }
 function m1() {
   return getdata()
@@ -16,60 +16,58 @@ function main() {
 }
 
 function run(func) {
-  const catchs = [], // 记录多个缓存
-    oldFetch = fetch // 记录原始的缓存，发送原始请求
+  // 重写 fetch
+  const oldFetch = fetch,
+    caches = []
   let i = 0
 
-  // 重写 fetch，在执行到 fetch('./data.js') 时，使用的就是这个重写的fetch
   window.fetch = (...args) => {
     // 判断缓存是否存在
-    if (catchs[i]) {
-      if (caches[i].status === 'success') {
-        return catchs[i].value
-      } else if (catchs[i].status === 'error') {
-        throw catchs[i].err
+    if (caches[i]) {
+      if (caches[i].status === 'fulfilled') {
+        return caches[i].values
+      } else if (caches[i].status === 'rejected') {
+        throw caches[i].error
       }
     }
 
+    // 发送真实请求
     const result = {
-      status: 'pending', // pending success error
+      status: 'pending',
       value: null,
-      err: null
+      error: null
     }
+
     // 设置 caches[0] = result, 然后 i++
-    caches[i++] = result
-    // 发送真是请求
-    const promise = oldFetch(...args)
+    caches[i] = result
+    ++i
+    const promise = fetch(...args)
       .then((res) => res.json())
       .then(
-        // 成功设置缓存
         (res) => {
-          result.status = 'success'
+          result.status = 'fulfilled'
           result.value = res
         },
-        // 失败 更改状态
         (err) => {
-          result.status = 'error'
-          result.err = err
+          result.status = 'rejected'
+          result.error = err
         }
       )
 
-    // 抛出错误
+    // 抛出错误，后面会捕获
     throw promise
   }
 
   try {
     func()
   } catch (error) {
-    // 这里的error 就是上面的 抛出的 promise
     if (error instanceof Promise) {
       const reRun = () => {
         i = 0
-        error()
+        func()
       }
-      error.then(reRun, reRun).finally(() => {
-        window.fetch = oldFetch
-      })
+      // 这里的error 就是上面抛出 promise
+      error.then(reRun, reRun).finally(() => (window.fetch = oldFetch))
     }
   }
 }
