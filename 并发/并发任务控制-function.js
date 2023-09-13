a087805384d78303ca8d2367417589d8769ad888
wangq <89414266@qq.com>
2023-09-13 10:18:54 +0800

    fix: 并发任务控制

---
+++ 并发/并发任务控制-function.js
@@ -0,0 +1,62 @@
+function asyncTask(time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(time)
+      resolve(time)
+    }, time * 1000)
+  })
+}
+
+const tasks = [
+  () => asyncTask(10),
+  () => asyncTask(5),
+  () => asyncTask(3),
+  () => asyncTask(4),
+  () => asyncTask(5)
+]
+
+function asyncFlowControl(tasks, limit) {
+  let results = [], // 记录任务结果
+    isRunning = 0, // 记录正在执行的任务数量
+    index = 0 // 当前执行任务的下标
+  return new Promise((resolve, reject) => {
+    function _run() {
+      if (index >= tasks.length || isRunning >= limit) {
+        // 达到并发限制或所有任务已执行完毕，返回结果
+        if (results.length === tasks.length) {
+          resolve(results)
+        }
+
+        return
+      }
+      let task = tasks[index]
+      const currentIndex = index // 保存当前下标
+
+      index++
+      isRunning++
+
+      task()
+        .then((result) => {
+          results[currentIndex] = result
+        })
+        .catch((error) => {
+          reject(error)
+        })
+        .finally(() => {
+          isRunning--
+          _run() // 递归执行下一个任务
+        })
+
+      _run() // 递归执行下一个任务
+    }
+    _run()
+  })
+}
+
+asyncFlowControl(tasks, 2)
+  .then((results) => {
+    console.log('All tasks completed:', results)
+  })
+  .catch((error) => {
+    console.error('Error occurred:', error)
+  })
