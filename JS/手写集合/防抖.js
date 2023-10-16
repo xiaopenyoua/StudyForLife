16f1d6c1959b97e0a0721f2d98a524033bfc61d7
wangq <89414266@qq.com>
2023-10-16 18:05:13 +0800

    fix: 防抖注释优化

--- JS/手写集合/防抖.js
+++ JS/手写集合/防抖.js
@@ -19,7 +19,7 @@ function debounce(fn, wait, immediate) {
       }
 
       timer = setTimeout(() => {
-        timer = null // timeout 作为闭包引用的上层函数的变量，是不会自动回收的。手动将其设置为 null ，让它脱离执行环境，一边垃圾收集器下次运行是将其回收。
+        timer = null // timer 作为闭包引用的上层函数的变量，是不会自动回收的。手动将其设置为 null ，让它脱离执行环境，以便垃圾收集器下次运行是将其回收。
       }, wait)
     } else {
       // 不立即执行的代码
